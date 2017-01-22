55611fb4cb6eee67c08157d0bdfde003ad9b110a
ScottT4ylor <G00236985@smail.raritanval.edu>
2017-01-22 08:12:48 -0500

    Made Start Screen Interface

---
+++ Assets/PlayOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayOnTouch : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider hit)
+    {
+        if(hit.gameObject.layer == 8)
+        {
+            print("play");
+            //go grab the Connect() function on the other script to start the game.
+        }
+    }
+}
